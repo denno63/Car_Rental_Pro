320cd179b2d47fb4258b0ebed42f109728772762
denno63 <mwaurakarobia2@gmail.com>
2026-08-05 08:01:07 +0300

    prepared backend and frontend for deployment

--- backend/run.py
+++ backend/run.py
@@ -1,15 +1,10 @@
-from app import create_app, db
+from app import create_app
 
 app = create_app()
 
-# Create tables if they don't exist
-@app.before_request
-def create_tables():
-    db.create_all()
-
 if __name__ == '__main__':
     app.run(
-        debug=True,
+        debug=False,
         host='0.0.0.0',
         port=5000
     )
